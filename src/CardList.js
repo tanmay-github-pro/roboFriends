d3665c764cc95ebb8463d8a7940a3ef1ce6d82b4
Tanmay <tanmayvashistha.mail@gmail.com>
2021-07-16 23:09:15 +0530

    Added unique key to each card as index is different while moving through the loop

--- src/CardList.js
+++ src/CardList.js
@@ -3,7 +3,7 @@ import Card from './Card';
 
 const CardList = ({robots}) => {
     const CardComponent = robots.map( (user, index) => {
-                return <Card id={user[index].id} name={user[index].name} email={user[index].email} />
+                return <Card key={index} id={user[index].id} name={user[index].name} email={user[index].email} />
         })
     return ( {CardComponent} );
 }
