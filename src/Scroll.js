d77357fa28e8e35039f205c95cec8be31729dd85
Tanmay <tanmayvashistha.mail@gmail.com>
2021-07-17 01:40:20 +0530

    Created a Scroll component as a wrapper for the CardList component

---
+++ src/Scroll.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Scroll = (props) => {
+    return (
+        // returning object within javascript expression
+        // that's why 2 curly brackets
+        <div style={{overflowY: 'scroll', border:'1px solid black', height: '500px'}}>
+            {props.children}
+        </div>
+    );
+};
+
+export default Scroll;
