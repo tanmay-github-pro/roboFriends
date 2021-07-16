9da67173432dfc4d0e7d0d00580a4b74f5325863
Tanmay <tanmayvashistha.mail@gmail.com>
2021-07-16 23:04:27 +0530

    made a component named "CardComponent" which takes robots array which was passed to CardList as a parameter and uses map() to return a card which holds name id and email and this is done on a loop

--- src/CardList.js
+++ src/CardList.js
@@ -2,13 +2,12 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({robots}) => {
+    const CardComponent = robots.map( (user, index) => {
+                return <Card id={user[index].id} name={user[index].name} email={user[index].email} />
+        })
     return (
         <div>
-            <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-            <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-            <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-            <Card id={robots[3].id} name={robots[4].name} email={robots[3].email} />
-            <Card id={robots[4].id} name={robots[4].name} email={robots[4].email} />
+            {CardComponent};
         </div>
     );
 }
