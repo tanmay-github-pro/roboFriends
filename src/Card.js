53e39be1d85a9d130bf12a0d39025bc82d5e99a6
Tanmay <tanmayvashistha.mail@gmail.com>
2021-07-16 22:49:38 +0530

    Destructured name, email, id "INSIDE THE PARAMETERS" using ES6 syntax

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-const Card = (props) => {
-    const { name, email, id} = props;
+const Card = ( { name, email, id} ) => {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow'>
             <img src = { `https://robohash.org/${id}?200x200` } alt='robot pic' />
