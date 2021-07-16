606e814eda90efcc52255883d9784d4a62e0d5b0
Tanmay <tanmayvashistha.mail@gmail.com>
2021-07-16 23:26:52 +0530

    Put everything directly into a div and return that

--- src/CardList.js
+++ src/CardList.js
@@ -2,10 +2,22 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({robots}) => {
-    const CardComponent = robots.map( (user, index) => {
-                return <Card key={index} id={user[index].id} name={user[index].name} email={user[index].email} />
-        })
-    return ( {CardComponent} );
+    return ( 
+        <div>
+            {
+                robots.map( (user, index) => {
+                    return (
+                        <Card 
+                            key={index} 
+                            id={user[index].id} 
+                            name={user[index].name} 
+                            email={user[index].email} 
+                        />
+                    );
+                })
+            }
+        </div>
+     );
 }
 
 export default CardList;
