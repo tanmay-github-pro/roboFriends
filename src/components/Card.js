4e239a4bf5272eef1e505f40d064f34358a30d6d
Tanmay <tanmayvashistha.mail@gmail.com>
2022-01-02 16:19:34 +0530

    Changed appearance of card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Card.css';
 
 const Card = ( { name, email, id} ) => {
     return (
-        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow'>
+        // <div className='tc bg-light-green dib br4 ma2 grow bw2 shadow card'>
+        <div className='tc dib br4 ma2 grow bw2 shadow card'>
             <img src = { `https://robohash.org/${id}?200x200` } alt='robot pic' />
-            <div>
+            <div className='card-info'>
                 <h2>{ name }</h2>
                 <p>{ email }</p>
             </div>
