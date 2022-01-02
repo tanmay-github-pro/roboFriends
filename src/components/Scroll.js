ed61256c2b6073dd534b2d551f2e1b16a3cf26dc
Tanmay <tanmayvashistha.mail@gmail.com>
2022-01-02 16:22:13 +0530

    changed height of scroll component and removed border

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -4,7 +4,7 @@ const Scroll = (props) => {
     return (
         // returning object within javascript expression
         // that's why 2 curly brackets
-        <div style={{overflowY: 'scroll', border:'1px solid black', height: '520px'}}>
+        <div style={{overflowY: 'scroll', height: '80vh'}}>
             {props.children}
         </div>
     );
