fb817e341da02ea7f2c160da34cca5218da54268
Tanmay <tanmayvashistha.mail@gmail.com>
2022-01-02 16:29:30 +0530

    Added a header tag and modified the look

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'
 import './App.css';
+import Search from '../components/Search';
 
 class App extends Component {
     constructor() {
@@ -16,29 +16,31 @@ class App extends Component {
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState( {robots: users} ))   
+            .then(users => this.setState({ robots: users }))
     }
 
     /* use arrow func as good practice when using custom methods
     else it throws some weird errors
     LOOk into this! */
     onSearchChange = (event) => {
-        this.setState( {searchField: event.target.value} )
-        
+        this.setState({ searchField: event.target.value })
+
     }
     render() {
         const { robots, searchField } = this.state;
-        const filteredRobots = robots.filter (
+        const filteredRobots = robots.filter(
             robottobefiltered => {
-            return robottobefiltered.name.toLowerCase()
+                return robottobefiltered.name.toLowerCase()
                     .includes(searchField.toLowerCase());
-        })
+            })
         return (
-            <div className = 'tc'>
-                <h1><strong>Robot Friends</strong></h1>
-                <SearchBox searchChange={this.onSearchChange} />
+            <div className='tc'>
+                <header className='App-header'>
+                    <h1 className='header-item' style={{color: 'black', padding: '0rem 1.5rem'}}>Robo Friends</h1>
+                    <Search className='header-item' searchChange={this.onSearchChange} />
+                </header>
                 <Scroll>
-                    <CardList robots = {filteredRobots}/>
+                    <CardList robots={filteredRobots} />
                 </Scroll>
             </div>
         );
